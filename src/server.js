7eb18a2359a888a59424b0df7b16d73397dc0f44
Rafael Bardini <rafael@rbardini.com>
2019-02-09 23:59:36 +0100

    Render to static node stream on the server

--- src/server.js
+++ src/server.js
@@ -2,9 +2,30 @@ import App from './App';
 import React from 'react';
 import { StaticRouter } from 'react-router-dom';
 import express from 'express';
-import { renderToString } from 'react-dom/server';
+import { renderToStaticNodeStream } from 'react-dom/server';
 
 const assets = require(process.env.RAZZLE_ASSETS_MANIFEST);
+
+const HTML = ({ assets, children }) => (
+  <html lang="en">
+    <head>
+      <meta charset="utf-8" />
+      <title>Pokédex</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      {assets.client.css
+        ? <link rel="stylesheet" href={assets.client.css} />
+        : ''
+      }
+      {process.env.NODE_ENV === 'production'
+        ? <script src={assets.client.js} defer></script>
+        : <script src={assets.client.js} defer crossorigin></script>
+      }
+    </head>
+    <body>
+      <div id="root">{children}</div>
+    </body>
+  </html>
+);
 
 const server = express();
 server
@@ -12,38 +33,20 @@ server
   .use(express.static(process.env.RAZZLE_PUBLIC_DIR))
   .get('/*', (req, res) => {
     const context = {};
-    const markup = renderToString(
-      <StaticRouter context={context} location={req.url}>
-        <App />
-      </StaticRouter>
+    const stream = renderToStaticNodeStream(
+      <HTML assets={assets}>
+        <StaticRouter context={context} location={req.url}>
+          <App />
+        </StaticRouter>
+      </HTML>
     );
 
     if (context.url) {
       res.redirect(context.url);
     } else {
-      res.status(200).send(
-        `<!doctype html>
-    <html lang="">
-    <head>
-        <meta charset="utf-8" />
-        <title>Pokédex</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        ${
-          assets.client.css
-            ? `<link rel="stylesheet" href="${assets.client.css}">`
-            : ''
-        }
-        ${
-          process.env.NODE_ENV === 'production'
-            ? `<script src="${assets.client.js}" defer></script>`
-            : `<script src="${assets.client.js}" defer crossorigin></script>`
-        }
-    </head>
-    <body>
-        <div id="root">${markup}</div>
-    </body>
-</html>`
-      );
+      res.setHeader('Content-Type', 'text/html; charset=utf-8')
+      res.write('<!DOCTYPE html>');
+      stream.pipe(res);
     }
   });
 
