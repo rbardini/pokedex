127ff4a102d62ee741c101a3157d101ef80ab4b9
Rafael Bardini <rafael@rbardini.com>
2019-02-09 17:28:00 +0100

    Move list data fetching to Home component

--- src/App/App.js
+++ src/App/App.js
@@ -1,37 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
 import Home from '../pages/Home';
 import Details from '../pages/Details';
 import './App.module.css';
 
-const App = () => {
-  const [pokemons, setPokemons] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const fetchPokemons = async () => {
-    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
-    const { results } = await response.json();
-    const pokemons = results.map((result, i) => ({
-      ...result,
-      // Workaround as there is no way to fetch sprite URLs without making one request per Pokémon :(
-      sprite: `//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`
-    }));
-
-    setPokemons(pokemons);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    fetchPokemons();
-  }, []);
-
-  return (
-    <Switch>
-      <Route exact path="/" component={() => <Home isLoading={isLoading} pokemons={pokemons} />} />
-      <Route exact path="/:name" component={Details} />
-    </Switch>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/:name" component={Details} />
+  </Switch>
+);
 
 export default App;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom'
 
 import styles from './Home.module.css';
 
-const Home = ({ isLoading, pokemons }) => (
-  <div className={styles.home}>
-    {isLoading ? (
-      <span>Loading...</span>
-    ) : (
-      <div className={styles.grid}>
-        {pokemons.map(({ name, sprite }) => (
-          <div className={styles.cell} key={name}>
-            <img src={sprite} alt={name} />
-            <Link to={`/${name}`}>{name}</Link>
-          </div>
-        ))}
-      </div>
-    )}
-  </div>
-);
+const Home = () =>  {
+  const [pokemons, setPokemons] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const fetchPokemons = async () => {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=151');
+    const { results } = await response.json();
+    const pokemons = results.map((result, i) => ({
+      ...result,
+      // Workaround as there is no way to fetch sprite URLs without making one request per Pokémon :(
+      sprite: `//raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i+1}.png`
+    }));
+
+    setPokemons(pokemons);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  return (
+    <div className={styles.home}>
+      {isLoading ? (
+        <span>Loading...</span>
+      ) : (
+        <div className={styles.grid}>
+          {pokemons.map(({ name, sprite }) => (
+            <div className={styles.cell} key={name}>
+              <img src={sprite} alt={name} />
+              <Link to={`/${name}`}>{name}</Link>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
 
 export default Home;
