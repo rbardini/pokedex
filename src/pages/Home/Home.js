ee5b9e770c54d2de46f42fc082ddc1068bea5f3a
Rafael Bardini <rafael@rbardini.com>
2019-02-09 13:29:15 +0100

    Convert Home to SFC

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,32 +2,28 @@ import React from 'react';
 import logo from '../../assets/react.svg';
 import styles from './Home.module.css';
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div className={styles.home}>
-        <div className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h2>Welcome to Razzle</h2>
-        </div>
-        <p className={styles.intro}>
-          To get started, edit <code>src/App.js</code> or{' '}
-          <code>src/Home.js</code> and save to reload.
-        </p>
-        <ul className={styles.resources}>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle">Docs</a>
-          </li>
-          <li>
-            <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
-          </li>
-          <li>
-            <a href="https://palmer.chat">Community Slack</a>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+const Home = () => (
+  <div className={styles.home}>
+    <div className={styles.header}>
+      <img src={logo} className={styles.logo} alt="logo" />
+      <h2>Welcome to Razzle</h2>
+    </div>
+    <p className={styles.intro}>
+      To get started, edit <code>src/App.js</code> or{' '}
+      <code>src/Home.js</code> and save to reload.
+    </p>
+    <ul className={styles.resources}>
+      <li>
+        <a href="https://github.com/jaredpalmer/razzle">Docs</a>
+      </li>
+      <li>
+        <a href="https://github.com/jaredpalmer/razzle/issues">Issues</a>
+      </li>
+      <li>
+        <a href="https://palmer.chat">Community Slack</a>
+      </li>
+    </ul>
+  </div>
+);
 
 export default Home;
