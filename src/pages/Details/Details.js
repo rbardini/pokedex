3303a0822d32099a40ba95afd0eb7481598ff6c5
Rafael Bardini <rafael@rbardini.com>
2019-02-09 17:18:04 +0100

    Show basic Pokémon details

--- src/pages/Details/Details.js
+++ src/pages/Details/Details.js
@@ -22,7 +22,39 @@ const Details = ({ match }) => {
       {isLoading ? (
         <span>Loading...</span>
       ) : (
-        <span>{JSON.stringify(pokemon)}</span>
+        <div>
+          <img src={pokemon.sprites.front_default} alt={pokemon.name}/>
+          <ul>
+            <li>Name: {pokemon.name}</li>
+            <li>Height: {pokemon.height / 10} m</li>
+            <li>Weight: {pokemon.weight / 10} kg</li>
+            <li>
+              Abilities:
+              <ul>
+                {pokemon.abilities.map(({ ability }) => <li key={ability.name}>{ability.name}</li>)}
+              </ul>
+            </li>
+            <li>Base experience: {pokemon.base_experience}</li>
+            <li>
+              Types:
+              <ul>
+                {pokemon.types.map(({ type }) => <li key={type.name}>{type.name}</li>)}
+              </ul>
+            </li>
+            <li>
+              Stats:
+              <ul>
+                {pokemon.stats.map(({ base_stat, stat }) => <li key={stat.name}>{stat.name} {base_stat}</li>)}
+              </ul>
+            </li>
+            <li>
+              Moves:
+              <ul>
+                {pokemon.moves.map(({ move }) => <li key={move.name}>{move.name}</li>)}
+              </ul>
+            </li>
+          </ul>
+        </div>
       )}
     </div>
   );
