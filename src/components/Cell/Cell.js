dea7c489ed7b205dd5f54898c13c53888d921c1b
Rafael Bardini <rafael@rbardini.com>
2019-02-10 12:06:25 +0100

    Only render Cell if props change

--- src/components/Cell/Cell.js
+++ src/components/Cell/Cell.js
@@ -1,13 +1,13 @@
-import React from 'react';
+import React, { memo } from 'react';
 import { Link } from 'react-router-dom'
 
 import styles from './Cell.module.css';
 
-const Cell = ({ pokemon }) => (
+const Cell = memo(({ pokemon }) => (
   <Link className={styles.cell} to={`/${pokemon.name}`}>
     <img className={styles.sprite} src={pokemon.sprite} alt="" />
     <span>{pokemon.formattedName}</span>
   </Link>
-);
+));
 
 export default Cell;
